6a65039e0ca339b771f99e7e02ed34fbf663bd68
Jason Hansen <jbhansen016@gmail.com>
2019-04-08 11:44:50 -0600

    finished the tutorial

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,28 +1,46 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import  React, { Component } from  'react';
+import { BrowserRouter } from  'react-router-dom' // BrowserRouter keeps the UI in sync with the URL using the HTML5 history API.
+// We use the Route component to define the routes of our application; 
+// the component the router should load once a match is found. 
+// Each route needs a path to specify the path to be matched and a component to specify the component to load. 
+// The exact property tells the router to match the exact path.
+import { Route, Link } from  'react-router-dom'
+import  UsersList  from  './UsersList'
+import  UserCreateUpdate  from  './UserCreateUpdate'
+import  './App.css';
 
-class App extends Component {
+const  BaseLayout  = () => (
+<div  className="container-fluid">
+    <nav  className="navbar navbar-expand-lg navbar-light bg-light">
+        <a  className="navbar-brand"  href="#">USmart Energy</a>
+        <button  className="navbar-toggler"  type="button"  data-toggle="collapse"  data-target="#navbarNavAltMarkup"  aria-controls="navbarNavAltMarkup"  aria-expanded="false"  aria-label="Toggle navigation">
+        <span  className="navbar-toggler-icon"></span>
+    </button>
+    <div  className="collapse navbar-collapse"  id="navbarNavAltMarkup">
+        <div  className="navbar-nav">
+            <a  className="nav-item nav-link"  href="/">USERS</a>
+            <a  className="nav-item nav-link"  href="/user">CREATE USER</a>
+        </div>
+    </div>
+    </nav>
+    <div  className="content">
+        <Route  path="/"  exact  component={UsersList}  />
+        <Route  path="/user/:user_id"  component={UserCreateUpdate}  />
+        <Route  path="/user/"  exact  component={UserCreateUpdate}  />
+    </div>
+</div>
+)
+
+// the root or top-level component of our React application:
+class  App  extends  Component {
+
   render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
+      return (
+      // We have wrapped the BaseLayout component with the BrowserRouter component since our app is meant to run in the browser.
+      <BrowserRouter>
+          <BaseLayout/>
+      </BrowserRouter>
+      );
   }
-}
-
-export default App;
+  }
+  export  default  App;
