5bfec75caa68cdb63416fc71ad96b38f3f3f44c9
Jason Hansen <jbhansen016@gmail.com>
2019-04-10 11:57:33 -0600

    switched db to raspberry pi postgresql, updated requirements.txt

--- usmart_energy_site/mysite/settings.py
+++ usmart_energy_site/mysite/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+import psycopg2
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -88,8 +89,12 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'test',
+        'USER': 'pi',
+        'PASSWORD': 'Electric',
+        'HOST': '155.97.234.94',
+        'PORT': '5432',
     }
 }
 
