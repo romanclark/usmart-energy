0142488c3fb1dd2d32ce182f6a56f8d70ec7fbae
Parker Stewart <pman@advanceddv.com>
2019-04-17 23:35:33 -0600

    Hopefully the seeder is working

--- usmart_energy_site/seeder.py
+++ usmart_energy_site/seeder.py
@@ -1,15 +1,19 @@
 import csv
 import os
+import random
 
 from users.models import User
 from assets.models import Asset
+from users.views import users_detail
 from transactions.models import Transaction
 
 # Seed users
+userCount = 0
+
 with open('seed_files/user_seed.csv') as csvfile:
     READER = csv.reader(csvfile)
     for row in READER:
-        p = User(
+        u = User(
             first_name=row[0],
             last_name=row[1],
             email=row[2],
@@ -21,15 +25,18 @@ with open('seed_files/user_seed.csv') as csvfile:
             longitude=row[8],
             inactive=row[9],
         )
-        p.save()
+        userCount += 1
+        u.save()
 
 
 # Seed Assets
 with open('seed_files/asset_seed.csv') as csvfile:
     READER = csv.reader(csvfile)
     for row in READER:
+        userID = random.randint(1, userCount)
+        user = User.objects.get(user_id=userID)
         p = Asset(
-            owner=row[0],
+            owner=user,
             nickname=row[1],
             asset_class=row[2],
             power=row[3],
@@ -47,12 +54,27 @@ with open('seed_files/asset_seed.csv') as csvfile:
 with open('seed_files/transactions_seed.csv') as csvfile:
     READER = csv.reader(csvfile)
     for row in READER:
+        userID1 = random.randint(1, userCount)
+        asset1 = Asset.objects.filter(owner_id=userID1, inactive = 'False')[:1].get()
+        
+        while not asset1:
+            userID1 = random.randint(1, userCount)
+            asset1 = Asset.objects.filter(owner_id=userID1, inactive = 'False')[:1].get()
+        
+        userID2 = random.randint(1, userCount)
+        asset2 = (Asset.objects.filter(owner_id=userID2, inactive = 'False')[:1]).get()
+
+        while not asset2:
+            userID2 = random.randint(1, userCount + 1)
+            asset2 = Asset.objects.filter(owner_id=userID2, inactive = 'False')[:1].get()
+
         p = Transaction(
-            buyer_asset_id = row[0],
-            seller_asset_id = row[1],
-            transaction_time = row[2],
-            energy_sent = row[3],
-            price_per_kwh = row[4],
+            buyer_asset_id = asset1,
+            seller_asset_id = asset2,
+            #transaction_time = row[2],
+            energy_sent = row[2],
+            price_per_kwh = row[3],
         )
+       
         p.save()
         
