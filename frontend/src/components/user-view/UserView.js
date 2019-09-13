1c07be4102681ee67bf721f1895918f321543acc
Jason Hansen <jbhansen016@gmail.com>
2019-09-12 18:22:25 -0600

    touching up stuff for asset creation

--- frontend/src/components/user-view/UserView.js
+++ frontend/src/components/user-view/UserView.js
@@ -1,10 +1,17 @@
 import React, { Component } from 'react';
-import UsersService from './UsersService';
 import { Container, Row, Col } from 'react-bootstrap';
+import Button from 'react-bootstrap/Button';
+import { FaUser, FaBolt } from 'react-icons/fa';
 
 import UserMonthlyStats from './UserMonthlyStats';
 import MapOfUser from './MapOfUser';
 import UserAssets from './UserAssets';
+
+import UsersService from './UsersService';
+import AssetsService from '../assets/AssetsService';
+
+const usersSerice = new UsersService();
+const assetsService = new AssetsService();
 
 class UserView extends Component {
 
@@ -13,6 +20,7 @@ class UserView extends Component {
 
         // bind functions
         this.getUserInfo = this.getUserInfo.bind(this);
+        this.getUserAssets = this.getUserAssets.bind(this);
 
         // set state
         this.state = {
@@ -25,19 +33,23 @@ class UserView extends Component {
             state: null,
             zipcode: null,
             latitude: null,
-            longitude: null
+            longitude: null,
+            numAssets: 0
         };
     }
 
     componentDidMount() {
         var self = this;
-        // TODO should grab user id from the parameters!!! and call getUserInfo
+        // grabbing user id from the parameters, 
+        // currently assigned in BaseContent.js with the : colon operator
+        const { match: { params } } = self.props;
+        self.getUserInfo(params.user_id);
+        self.getUserAssets(params.user_id);
     }
 
     getUserInfo(user_id) {
         var self = this;
-        UsersService.getUser(user_id).then(function (result) {
-            console.log(result);
+        usersSerice.getUser(user_id).then(function (result) {
             self.setState({
                 user_id: result.user_id,
                 first_name: result.first_name,
@@ -53,25 +65,37 @@ class UserView extends Component {
         })
     }
 
+    getUserAssets(user_id) {
+        var self = this;
+        assetsService.getAssetsByUser(user_id).then(function (result) {
+            self.setState({
+                numAssets: result.count
+            })
+        })
+    }
+
     render() {
+        var hey = "hey";
         return (
             <div className="user--view container">
                 <p className="page-title">Homeowner View</p>
                 <Container>
                     <Row>
                         <Col>
-                            <p>User monthly stats</p>
-                            {/* TODO pass relevant user info here */}
+                            {/* TODO add in the user stats */}
+                            {/* <p>User monthly stats</p> */}
+                            {/* pass relevant user info here */}
                             {/* <UserMonthlyStats></UserMonthlyStats> */}
                         </Col>
                     </Row>
                     <Row>
                         <Col className="wrapper">
-                            <p>My profile</p>
-                            <p>Name:</p>
-                            <p>Address:</p>
-                            <p>Number of asssets:</p>
-                            <p>Edit your profile</p>
+                            <p className="page-subtitle">My Profile</p>
+                            <p>Name: {this.state.first_name} {this.state.last_name}</p>
+                            <p>Address: {this.state.street}, {this.state.city}, {this.state.state}, {this.state.zipcode}</p>
+                            <p>Number of asssets: {this.state.numAssets}</p>
+                            <Button variant="outline-secondary" href={"/users/" + this.state.user_id}><FaUser /> Edit My Account</Button>
+                            <Button variant="outline-secondary" href={"/asset/" + this.state.user_id}>Add A New Asset <FaBolt /></Button>
                         </Col>
                         <Col className="wrapper">
                             <MapOfUser></MapOfUser>
@@ -79,9 +103,9 @@ class UserView extends Component {
                     </Row>
                     <Row>
                         <Col className="wrapper">
-                            <p>My assets</p>
+                            <p>My Assets</p>
                             <p>Add a new asset</p>
-                            {/* <UserAssets></UserAssets> */}
+                            {/* <UserAssets user_id={this.state.user_id}></UserAssets> */}
                         </Col>
                     </Row>
                 </Container>
