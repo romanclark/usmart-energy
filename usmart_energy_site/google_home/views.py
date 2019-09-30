e2923b30f786f2989adbf04d55ea818769e3fa38
Parker Stewart <pman@advanceddv.com>
2019-09-30 13:03:39 -0600

    Updated deadline method, added two more fullfilments

--- usmart_energy_site/google_home/views.py
+++ usmart_energy_site/google_home/views.py
@@ -3,6 +3,7 @@ from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 import json
 import requests
+from datetime import datetime
 
 
 @csrf_exempt
@@ -20,8 +21,11 @@ def webhook(request):
     user_id = user_info.get('sub')
 
     # Define paths for each action to follow
-    if action == 'list_active':
-        pass
+    if action == 'tellDevices':
+        return active_devices(user_id)
+    elif action == 'set_to_available':
+        device = req.get('queryResult').get('parameters').get('device_nickname')
+        return set_to_available(user_id, device)
     elif action == 'update_deadline':
         device = req.get('queryResult').get('parameters').get('device_nickname')
         deadline = req.get('queryResult').get('parameters').get('deadline')
@@ -33,17 +37,73 @@ def webhook(request):
     return JsonResponse(fulfillment_text, safe=False)
 
 
+def set_to_available(user_id, asset_name):
+    try:
+        device = Asset.objects.get(owner=user_id, nickname__iexact=asset_name)
+    except Asset.DoesNotExist:
+        return JsonResponse({'fulfillmentText': 'Unable to find matching asset, please try again.'}, safe=False)
+
+    if device.available:
+        return JsonResponse({'fulfillmentText': 'This asset is already available.'}, safe=False)
+    else:
+        device.available = True
+        device.save()
+        return JsonResponse({'fulfillmentText': 'Ok, your device, {}, is now available.'.format(asset_name)}, safe=False)
+
+#  Get active devices
 def active_devices(user_id):
-    pass
+    try:
+        devices = Asset.objects.filter(owner=user_id, available=True)
+    except Asset.DoesNotExist:
+        return JsonResponse({'fulfillmentText': 'You do not have any active devices'}, safe=False)
+
+    devString = ""
+    counter = 1
+    
+    for dev in devices:
+        if counter == devices.count():
+            devString += ", and " + dev.nickname
+            break
+        devString += dev.nickname + ", "
+        counter += 1
+
+    return JsonResponse({'fulfillmentText': 'Here are your active devices: {}.'.format(devString)}, safe=False)
 
 
 def update_deadline(user_id, asset_name, deadline):
     try:
-        asset = Asset.objects.get(owner=user_id, nickname=asset_name)
+        asset = Asset.objects.get(owner=user_id, nickname__iexact=asset_name)
     except Asset.DoesNotExist:
         return JsonResponse({'fulfillmentText': 'Unable to find matching asset, please try again.'}, safe=False)
 
-    asset.user_deadline = deadline
+    # TODO ADD A LOT OF LOGIC
+
+    if type(deadline) is not dict:
+        return JsonResponse({'fulfillmentText': 'Unable to update deadline, please specify a day for your deadline.'}, safe=False)
+
+    # When google is told a date and time, it sends it as a dictionary (No clue why)
+    # So to ensure we get a datetime, I check to see if they sent us a dictionary, and
+    # if they don't then I ask them to specify a day ^^
+
+    # Here, we split on the T in the date-time that is sent to us, to parse the date
+    split_dl = deadline['date_time'].split("T")
+
+    # The time comes in a weird  HH:MM:SS-00.00 format, and we don't care about the stuff after the dash.
+    time_split = split_dl[1].split("-")
+    # Now get the hour, minute, and seconds to update later
+    time_split = time_split[0].split(":")
+
+    # Google always sends it in this format, for parse using this format and update
+    date = datetime.strptime(split_dl[0], '%Y-%m-%d')
+
+    date = date.replace(hour=int(time_split[0]), minute=int(time_split[1]), second=0, microsecond=0)
+
+    print(date)
+    print(datetime.now())
+    if date < datetime.now():  # index in the dict to get the actual datetime
+        return JsonResponse({'fulfillmentText': 'I am sorry, you cannot change your deadline to the past'}, safe=False)
+
+    asset.user_deadline = date
     asset.save()
 
     return JsonResponse({'fulfillmentText': 'Ok, deadline for {} updated.'.format(asset_name)}, safe=False)
